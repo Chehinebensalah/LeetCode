bb50f960940eef56c27611fcc00a0f894ba27b88
Chehinebensalah <87309689+Chehinebensalah@users.noreply.github.com>
2024-01-09 08:38:04 +0100

    Time: 6 ms (22.71%), Space: 13.3 MB (49.18%) - LeetHub

---
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> s,s1;
+    void getleaf(TreeNode * root,vector<int>& s){
+        if(root->right==nullptr && root->left==nullptr){
+            s.push_back(root->val);
+            return ; 
+        }
+        if(root->right!=nullptr){
+            getleaf(root->right,s);
+        }
+        if(root->left!=nullptr){
+            getleaf(root->left,s);
+        }
+    }
+    
+    bool leafSimilar(TreeNode* root1, TreeNode* root2) {
+        s.clear();
+        s1.clear();
+        getleaf(root1,s);
+        getleaf(root2,s1);
+        if(s.size()!=s1.size()){
+            return false;
+        }
+        
+        for(int i=0;i<s.size();i++){
+            if(s[i]!=s1[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
