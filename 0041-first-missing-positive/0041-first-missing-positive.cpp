dba98c513367387c30a378655ef271d61f5e9450
Chehinebensalah <87309689+Chehinebensalah@users.noreply.github.com>
2024-03-26 09:02:04 +0100

    Time: 79 ms (15.71%), Space: 63 MB (6.19%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -3,27 +3,12 @@ public:
     int firstMissingPositive(vector<int>& nums) {
         int n = nums.size() , ans=1;
         sort(nums.begin(),nums.end());
-        if(nums[n-1]<=0){
-            return ans;
-        }
-        int i=0;
-        while(i<n){
-            if(nums[i]<=0){
-                nums.erase(nums.begin()+i);
-            }
-            else{
-                i++;
-            }
-        }
-        if(nums[0]!=1){
-            return ans;
-        }
         vector<int> h ;
         unordered_map<int,bool> vis;
+        
+        
         for(int i=0;i<n;i++){
-            if(vis.find(nums[i])!=vis.end()){
-                continue;
-            }else{
+            if(nums[i]>=1 && !(vis.find(nums[i])!=vis.end())){
                 vis[nums[i]]=true;
                 h.push_back(nums[i]);
             }
