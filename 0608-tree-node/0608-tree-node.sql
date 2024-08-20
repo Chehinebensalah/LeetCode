3b23de7926347b79c4fc4b822200b1c14018529d
Chehinebensalah <87309689+Chehinebensalah@users.noreply.github.com>
2024-08-20 15:48:13 +0100

    Time: 1032 ms (5.31%), Space: 0B (100%) - LeetHub

--- 0608-tree-node/0608-tree-node.sql
+++ 0608-tree-node/0608-tree-node.sql
@@ -2,7 +2,7 @@ SELECT
     id,
     CASE 
         WHEN p_id is null THEN 'Root'
-        WHEN id in (select distinct(p_id) from Tree) THEN 'Inner'
+        WHEN id in (select p_id from Tree) THEN 'Inner'
         ELSE 'Leaf' 
     END AS [type]
 FROM 
