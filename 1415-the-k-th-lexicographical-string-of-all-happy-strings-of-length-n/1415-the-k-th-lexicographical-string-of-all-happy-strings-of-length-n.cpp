7b9a20acee2f3480a5442999139a1aa5f204c2e4
Chehinebensalah <87309689+Chehinebensalah@users.noreply.github.com>
2025-02-19 11:05:49 +0100

    Time: 411 ms (5.05%), Space: 52.4 MB (5.05%) - LeetHub

--- 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -6,28 +6,29 @@ public:
         q.push("b");
         q.push("c");
         string s = q.front();
-        vector<string> v = {"a","b","c"};
-        if(n==1 && k<=3) return v[k-1];
-        else v.clear();
+        vector<string> v = {"a", "b", "c"};
+        if (n == 1 && k <= 3)
+            return v[k - 1];
+        else
+            v.clear();
         while (!q.empty()) {
             q.pop();
-            if(s.size()>n)
+            if (s.size() > n)
                 break;
             for (auto a : {'a', 'b', 'c'}) {
                 if (s[s.size() - 1] != a) {
                     q.push(s + a);
-                    if((s+a).size() == n){
-                        v.push_back(s+a);
+                    if ((s + a).size() == n) {
+                        v.push_back(s + a);
                     }
                 }
             }
             s = q.front();
-            
         }
-        
-        sort(v.begin(),v.end());
-        for(int i=0;i<v.size();i++) cout<<v[i]<<endl;
-        if(n*3 < k ) return "";
-        return v[k-1];
+
+        sort(v.begin(), v.end());
+        for (int i = 0; i < v.size(); i++)
+            cout << v[i] << endl;
+        return (v.size() < k ? "" : v[k - 1]);
     }
 };
