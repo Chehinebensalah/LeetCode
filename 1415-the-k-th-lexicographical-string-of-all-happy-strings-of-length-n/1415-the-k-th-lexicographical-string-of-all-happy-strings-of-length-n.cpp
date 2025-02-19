5e659aea9df7ec6165a0020b103e1ac62d6a55ae
Chehinebensalah <87309689+Chehinebensalah@users.noreply.github.com>
2025-02-19 10:55:42 +0100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    string getHappyString(int n, int k) {
+        queue<string> q;
+        q.push("a");
+        q.push("b");
+        q.push("c");
+        string s = q.front();
+        vector<string> v = {"a","b","c"};
+        if(n==1 && k<=3) return v[k-1];
+        else v.clear();
+        while (!q.empty()) {
+            q.pop();
+            if(s.size()>n)
+                break;
+            for (auto a : {'a', 'b', 'c'}) {
+                if (s[s.size() - 1] != a) {
+                    q.push(s + a);
+                    if((s+a).size() == n){
+                        v.push_back(s+a);
+                    }
+                }
+            }
+            s = q.front();
+            
+        }
+        
+        sort(v.begin(),v.end());
+        for(int i=0;i<v.size();i++) cout<<v[i]<<endl;
+        if(n*3 < k ) return "";
+        return v[k-1];
+    }
+};
