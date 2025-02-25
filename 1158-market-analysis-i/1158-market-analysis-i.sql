91ad98c9855f3f2d05f10851fdada2e4d3f66b87
Chehinebensalah <87309689+Chehinebensalah@users.noreply.github.com>
2025-02-25 10:30:47 +0100

    Time: 3458 ms (48.75%), Space: 0B (100%) - LeetHub

--- 1158-market-analysis-i/1158-market-analysis-i.sql
+++ 1158-market-analysis-i/1158-market-analysis-i.sql
@@ -5,9 +5,8 @@ with cte as (
     where YEAR(order_date) = 2019
     group by buyer_id
 )
-
 select  u.user_id as buyer_id,
         u.join_date,
-        count(c.orders_in_2019,0) as orders_in_2019
+        ISNULL(c.orders_in_2019,0) as orders_in_2019
 from users u 
 left join cte c on u.user_id = c.buyer_id
